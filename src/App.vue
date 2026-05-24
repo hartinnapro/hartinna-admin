4d1720cd26ea45e1d51dd949e2ef5213d9e09648
Hartinna Pro <hartinnapro@gmail.com>
2026-05-25 01:38:30 +0800

    fix: restore useWakeLock call in admin App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,8 @@
+<script setup>
+import { useWakeLock } from '@/composables/useWakeLock'
+useWakeLock()
+</script>
+
 <template>
   <RouterView v-slot="{ Component }">
     <KeepAlive :exclude="['LoginView']">
